bb2823dd44583ca72d8a764e5cd144780e923049
martim98 <martim.ferreira.almeida@gmail.com>
2020-06-30 15:33:47 +0100

    centering corrigido

--- script_parte2.R
+++ script_parte2.R
@@ -47,28 +47,7 @@ boxplot(normalized_bg_patient1@spotData$Cy5, normalized_bg_patient2@spotData$Cy5
         main = 'Cy5 Normalized intensities')
 axis(1, at=1:3, labels=c('Chip 1', 'Chip 2', 'Chip 3'))
 
-## Centering
-
-# normalized_bg_patient1@spotData$Cy3 <- scale(normalized_bg_patient1@spotData$Cy3) 
-# normalized_bg_patient1@spotData$Cy5 <- scale(normalized_bg_patient1@spotData$Cy5)
-# 
-# normalized_bg_patient2@spotData$Cy3 <- scale(normalized_bg_patient2@spotData$Cy3) 
-# normalized_bg_patient2@spotData$Cy5 <- scale(normalized_bg_patient2@spotData$Cy5)
-# 
-# 
-# normalized_bg_patient3@spotData$Cy3<- scale(normalized_bg_patient3@spotData$Cy3) 
-# normalized_bg_patient3@spotData$Cy5 <- scale(normalized_bg_patient3@spotData$Cy5)
-
-## colocar num data frame
-# data_centered <- data.frame(centered_patient1_cy3, 
-#                             centered_patient1_cy5,
-#                             centered_patient2_cy3,
-#                             centered_patient2_cy5,
-#                             centered_patient3_cy3,
-#                             centered_patient3_cy5)
-
-## Zscore
-
+## zscores
 zscores1 <- Zscore(normalized_bg_patient1, type = 'ma')
 zscores2 <- Zscore(normalized_bg_patient2, type = 'ma')
 zscores3 <- Zscore(normalized_bg_patient3, type = 'ma')
@@ -77,41 +56,46 @@ Zscore.plot(zscores1)
 Zscore.plot(zscores2)
 Zscore.plot(zscores3)
 
-## Package limma precisa de uma matrix com os cy3 e com os cy5
+## Centering com log, como nos slides?
+## comentado pois estava a causar problemas com o calculo dos Z scores
 
 
-data_cy3 <- data.frame('Cy3.1' = normalized_bg_patient1@spotData$Cy3,
+
+
+normalized_bg_patient1@spotData$Cy3 <- scale(normalized_bg_patient1@spotData$Cy3, 2) 
+normalized_bg_patient1@spotData$Cy5 <- scale(normalized_bg_patient1@spotData$Cy5, 2)
+#
+normalized_bg_patient2@spotData$Cy3 <- scale(normalized_bg_patient2@spotData$Cy3, 2) 
+normalized_bg_patient2@spotData$Cy5 <- scale(normalized_bg_patient2@spotData$Cy5, 2)
+
+normalized_bg_patient3@spotData$Cy3<- scale(normalized_bg_patient3@spotData$Cy3, 2) 
+normalized_bg_patient3@spotData$Cy5 <- scale(normalized_bg_patient3@spotData$Cy5, 2)
+
+
+
+## Package limma precisa de um matrix like data com os cy3 e com os cy5
+
+
+data <- data.frame('Cy3.1' = normalized_bg_patient1@spotData$Cy3,
                        'Cy3.2' = normalized_bg_patient2@spotData$Cy3,
-                       'Cy3.3' = normalized_bg_patient3@spotData$Cy3)
-data_cy3 <- as.matrix(data_cy3)
-data_cy5 <- data.frame('Cy5.1' = normalized_bg_patient1@spotData$Cy5,
+                       'Cy3.3' = normalized_bg_patient3@spotData$Cy3,
+                       'Cy5.1' = normalized_bg_patient1@spotData$Cy5,
                        'Cy5.2' = normalized_bg_patient2@spotData$Cy5,
                        'Cy5.3' = normalized_bg_patient3@spotData$Cy5)
-data_cy5 <- as.matrix(data_cy5)
 
+## O design muda com a mudança da cor, logo 3x -1 e 3x 1, de acordo com os slides
 
-data <- read.maimages(files = c(path1, path2, path3))
-
-
-
-
-
-
-
-
-
-
-obj <- list(M = '', A = '')
-obj
-class(obj) <- 1
-obj$M <- data_cy3
-obj$A <- data_cy5
-
-
-
-fit <- lmFit(obj)
+fit <- lmFit(data, design = c(-1, -1, -1, 1, 1, 1))
 b_fit <- eBayes(fit)
-b_fit
+topTable(b_fit, genelist = normalized_bg_patient1@spotData$Id)
 
-table <- topTable(b_fit,number=20,adjust="BH")
-table
+## valores p muito altos, algo não está bem ?!
+
+volcanoplot(b_fit)
+
+
+
+
+
+
+
